7be071f265ea5e2bfb9aa8b5d7104b2473b95bbd
bryanparmelee <bryan.parmelee@gmail.com>
2022-10-15 11:59:15 -0400

    Add trending API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const TRENDING_API = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
+
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  useEffect(() => {
+    getMovies(TRENDING_API);
+  }, []);
+
   return (
     <div className="App">
      Hello world
