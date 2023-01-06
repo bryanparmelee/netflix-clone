4e3ce36d1b8368c86e46e554c8c67830b14ade25
bryanparmelee <bryan.parmelee@gmail.com>
2023-01-06 00:16:39 -0500

    Add volume icon and rating to featured movie

--- src/components/featured/featured.component.jsx
+++ src/components/featured/featured.component.jsx
@@ -1,5 +1,6 @@
 import play from '../../assets/play-24.png';
 import info from '../../assets/info-24.png';
+import speaker from '../../assets/speaker-24.png';
 
 const Feature = ({ backdrop_path, title, overview }) => {
 
@@ -12,28 +13,45 @@ const Feature = ({ backdrop_path, title, overview }) => {
                 src={featuredImgPath}
                 className="w-full object-contain"    
                 />
-            <div className="w-1/2 h-full absolute top-1/2 left-0 z-10 text-white px-8">
+            <div className="w-full h-full absolute top-1/2 left-0 z-10 text-white pl-8">
                 <h2 className='text-4xl font-medium'>{title}</h2>
-                <div className="w-full mt-12 flex gap-4">
-                    <button className="w-24 h-10 bg-white text-black rounded">
-                        <div className='w-full h-full flex align-center justify-center p-2.5 gap-2'>
+                <div className="w-full mt-12 flex justify-between">
+                    <div className="w-auto flex gap-4">
+                        <button className="w-24 h-10 bg-white text-black rounded"
+                                type='button'
+                        >
+                            <div className='w-full h-full flex items-center justify-center p-2.5 gap-2'>
+                                <img 
+                                    src={play}
+                                    alt='play'                         
+                                />                            
+                                <span className="font-medium">Play</span>
+                            </div>
+                        </button>
+                        <button className="w-auto h-10 bg-slate-500/50 rounded"
+                                type='button'
+                        >
+                            <div className='w-full h-full flex items-center justify-center p-2.5 gap-2'>
+                                <img 
+                                    src={info}
+                                    alt='info'
+                                />
+                                <span className="text-white font-medium">More Info</span>
+                            </div>                            
+                        </button>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                        <div className='w-8 h-8 border rounded-full flex items-center justify-center'>
                             <img 
-                                src={play}
-                                alt='play'                         
-                            />                            
-                            <span className="font-medium">Play</span>
+                                className="w-5"
+                                alt='volume' 
+                                src={speaker}
+                            />                           
                         </div>
-                    </button>
-                    <button className="w-auto h-10 bg-slate-400/50 rounded">
-                        <div className='w-full h-full flex align-center justify-center p-2.5 gap-2'>
-                            <img 
-                                src={info}
-                                alt='info'
-                            />
-                            <span className="text-white font-medium">More Info</span>
+                        <div className='w-36 h-9 border-l-2 bg-black/50 ml-4 flex items-center pl-2'>
+                                <span className='text-white font-medium'>TV-MA</span>
                         </div>
-                        
-                    </button>
+                    </div>
                 </div>
                 {/* <h4 className='font-lg font-light'>{overview}</h4> */}
             </div>
